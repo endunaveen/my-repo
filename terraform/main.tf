96f9afa5da3e26b1b7e46c10c6e37372b8cad505
endunaveen <104223330+endunaveen@users.noreply.github.com>
2025-07-19 19:21:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,62 +1,14 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
-resource "aws_key_pair" "ec2_key" {
-  key_name   = "ec2-2-key"
-  public_key = file("~/.ssh/ec2-2-key.pub")
-}
-
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-2-sg"
-  description = "Allow SSH, HTTP, HTTPS, Tomcat"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "ec2" {
-  ami                    = "ami-0c02fb55956c7d316" # Ubuntu 22.04
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.ec2_key.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
-  associate_public_ip_address = true
+resource "aws_instance" "ec2_2" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name                    # EXISTING key
+  vpc_security_group_ids      = [var.security_group_id]         # EXISTING SG
 
   tags = {
-    Name = "ec2-2-kind"
+    Name = "ec2-2"
   }
 }
-
