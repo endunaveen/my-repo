89c9e7880c22d79e7ef0a0306ee05cacfdaaf1a8
endunaveen <104223330+endunaveen@users.noreply.github.com>
2025-07-19 16:15:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,16 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_key_pair" "ec2_2_key" {
+resource "aws_key_pair" "ec2_key" {
   key_name   = "ec2-2-key"
   public_key = file("~/.ssh/ec2-2-key.pub")
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-2-sg"
+  description = "Allow SSH, HTTP, HTTPS, Tomcat"
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -20,7 +19,20 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   ingress {
-    description = "Tomcat (8080)"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -35,43 +47,16 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-resource "aws_instance" "ec2_2" {
-  ami                         = "ami-0c02fb55956c7d316" # Ubuntu 22.04 LTS
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.ec2_2_key.key_name
-  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0c02fb55956c7d316" # Ubuntu 22.04
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.ec2_key.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
   associate_public_ip_address = true
 
   tags = {
-    Name = "ec2-2-microk8s"
+    Name = "ec2-2-kind"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      "sudo apt install -y snapd",
-      "sudo snap install microk8s --classic",
-      "sudo usermod -a -G microk8s ubuntu",
-      "sudo chown -f -R ubuntu ~/.kube",
-      "newgrp microk8s",
-      "sudo microk8s status --wait-ready",
-      "sudo microk8s enable helm3",
-      "sudo microk8s helm3 repo add bitnami https://charts.bitnami.com/bitnami",
-      "sudo microk8s helm3 install tomcat bitnami/tomcat"
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/ec2-2-key")
-      host        = self.public_ip
-    }
-  }
-}
-
-
-output "ec2_2_public_ip" {
-  description = "Public IP of EC2-2 instance"
-  value       = aws_instance.ec2_2.public_ip
 }
 
