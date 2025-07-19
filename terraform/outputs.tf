24efa2de1bf6e787a9c6c18d4428fe258e0a1e26
endunaveen <104223330+endunaveen@users.noreply.github.com>
2025-07-19 19:23:30 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
-output "ec2_public_ip" {
-  value = aws_instance.ec2.public_ip
+output "ec2_2_public_ip" {
+  description = "Public IP of EC2-2 instance"
+  value       = aws_instance.ec2_2.public_ip
+}
+
+output "ec2_2_private_ip" {
+  description = "Private IP of EC2-2 instance"
+  value       = aws_instance.ec2_2.private_ip
 }
